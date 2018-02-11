59c9009cb847fe22dd0bc37c72741004d8a46a0e
Rémi Morillon <rmorillon@freneticexperience.com>
2018-02-11 12:14:50 +0100

    Add ICompositeConfigurator

---
+++ ICompositeConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Diese.Modelization
+{
+    public interface ICompositeConfigurator<in T> : IConfigurator<T>
+    {
+        new IEnumerable<IConfigurator<T>> SubConfigurators { get; }
+    }
+
+    public interface ICompositeConfigurator<in T, out TSubConfigurator> : ICompositeConfigurator<T>
+        where TSubConfigurator : IConfigurator<T>
+    {
+        new IEnumerable<TSubConfigurator> SubConfigurators { get; }
+    }
+}
